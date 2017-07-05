25b6daf4063e81a18dd39c0993b5fa2f162c5719
mraju2 <search4gyan@gmail.com>
2017-07-05 16:29:07 +0530

    Create Globals.cs

---
+++ SourceCode/VILMS_Layers/VILMS_CM/Globals.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VILMS_CM
+{
+    public static class Globals
+    {
+        #region Variables
+        public static string connectionString;
+        public static string dbname = "\\Database\\vilms.db";
+        public static string UserId;
+        #endregion
+    }
+}
